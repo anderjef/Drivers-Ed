b3a3988ff6bd2dfccba8cdb2463090161e1820c1
jeffreyandersen <jeffreyandersen@tamu.edu>
2019-11-13 18:03:22 -0600

    original upload - part 1 and start of part 2

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 carMove;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        carMove = Vector3.zero;
+        carMove.x = Input.GetAxisRaw("Horizontal") * speed;
+        carMove.z = speed;
+        controller.Move(carMove * Time.deltaTime);
+    }
+}
